d9b5743ad7dd8f783975afba2e158f1993207b79
Dayamond <pyramond69@gmail.com>
2025-10-01 23:42:53 +0200

    feat: create header

--- locales/en.ts
+++ locales/en.ts
@@ -1,3 +1,5 @@
 export default {
-    "name": "Quentin T'JAMPENS"
+    "name": "Quentin T'JAMPENS",
+    "title": "Junior developer",
+    "sub-title": "Computer Science student"
 } as const
--- locales/fr.ts
+++ locales/fr.ts
@@ -1,3 +1,5 @@
 export default {
-  "name": "Quentin T'JAMPENS"
+    "name": "Quentin T'JAMPENS",
+    "title": "Développeur junior",
+    "sub-title": "Etudiant en informatique - BUT2"
 } as const
