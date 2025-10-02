4a79a00f3b4d387739b42c2822eeb14a28ba3031
Dayamond <pyramond69@gmail.com>
2025-10-02 19:53:05 +0200

    feat: responsive design

--- components/home/header.tsx
+++ components/home/header.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import {TypingAnimation} from "@/components/ui/typing-animation";
-import {getI18n} from "@/locales/server";
+import { TypingAnimation } from "@/components/ui/typing-animation";
+import { getI18n } from "@/locales/server";
 
 const Header: React.FC = async () => {
 
@@ -8,9 +8,9 @@ const Header: React.FC = async () => {
 
     return (
         <div className="font-sans relative min-h-screen p-8 pb-20 sm:p-20 flex flex-col items-center gap-5">
-            <h1 className="text-7xl font-oswald"> {t("name")} </h1>
-            <TypingAnimation className="text-4xl" words={[t("title")]} showCursor={false} typeSpeed={50} />
-            <TypingAnimation className="text-3xl" words={[t("sub-title")]} showCursor={false} typeSpeed={50} />
+            <h1 className="text-4xl font-oswald sm:text-7xl"> {t("name")} </h1>
+            <TypingAnimation className="text-2xl sm:text-4xl" words={[t("title")]} showCursor={false} typeSpeed={50} />
+            <TypingAnimation className="text-2xl sm:text-3xl" words={[t("sub-title")]} showCursor={false} typeSpeed={50} />
         </div>
     )
 }
