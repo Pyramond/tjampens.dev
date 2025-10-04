19f0dfe74813a5940aede751fa71ee802a5a86f0
Dayamond <pyramond69@gmail.com>
2025-10-04 23:12:29 +0200

    style: fix background cut

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -7,19 +7,17 @@ import Projects from "@/components/home/projects";
 import Skills from "@/components/home/skills";
 
 export default async function Home() {
-    const t = await getI18n();
 
-    return (
-        <div className="relative w-full">
-            <LightRays className="absolute inset-0 h-full w-full -z-10 pointer-events-none" />
-            <Meteors number={30} />
+  const t = await getI18n();
 
-            <div className="relative flex min-h-[100svh] flex-col items-center gap-5">
-                <Header />
-                <GeneralInformations />
-                <Projects />
-                <Skills />
-            </div>
-        </div>
-    );
+  return (
+    <div className="flex flex-col items-center gap-5 relative overflow-hidden">
+        <Meteors number={30}/>
+        <LightRays />
+        <Header />
+        <GeneralInformations />
+        <Projects />
+        <Skills />
+    </div>
+  );
 }
